ae529c50810f6ea716f9b9def86b458a2fa7f319
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-02 22:01:19 -0800

    backend update

--- kwitter_room.js
+++ kwitter_room.js
@@ -11,11 +11,29 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+function addRoom(){
+  room_name = document.getElementById("room_name").value;
+  firebase.database().ref("/").child(room_name).update({
+    purpose: "adding room name"
+  });
+  localStorage.setItem("room_name",room_name);
+  window.location = "kwitter_page.html";
+}
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+}
        Room_names = childKey;
       //Start code
-
+      console.log("Room name - "+Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick = 'redirectToRoom(this.id)'>#"+Room_names+"</div> <hr>";
+      document.getElementById("output").innerHTML += row;
       //End code
       });});}
 getData();
+function redirectToRoom(name){
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html";
+}
